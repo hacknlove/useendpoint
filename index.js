0df2117622d6ee2b5d4fefa3ecc48e87d5a761ee
antonio <antonio@pykiss.com>
2019-08-09 11:32:42 +0200

    first version

---
+++ index.js
@@ -0,0 +1,37 @@
+const { useState, useEffect } = require('react')
+const isDifferent = require('isdifferent')
+const fetchHelper = require('@hacknlove/fetchhelper')
+/**
+ * Does the fetch and sets the new value if isDifferent
+ * @param {array [url, options]} fethParameters parameter for call the fetch function
+ * @param {*} value current value
+ * @param {function} set function to set the new value
+ */
+async function checkEndPoint (fethParameters, value, set) {
+  const [response, error] = await fetchHelper(fethParameters)
+  if (isDifferent(value, [response, error])) {
+    set([response, error])
+  }
+}
+
+/**
+ * Fetch a url periodically and update the value if changes
+ * @param {array [url, options]} fethParameters parameter for call the fetch function
+ * @param {*} first First value
+ * @param {function} interval, the interval between fetches
+ */
+function useEndpoint (fethParameters, first, interval = 3000) {
+  const [value, set] = useState([first])
+
+  function refresh () {
+    checkEndPoint(fethParameters, value, set)
+  }
+  refresh()
+  useEffect(() => {
+    const interval = setInterval(refresh, 30000)
+    return () => clearInterval(interval)
+  })
+  return [value[0], refresh, value[1]]
+}
+
+module.exports = useEndpoint
